a1b2f43c69a873d12621346ddfffefbc3a1a688b
Micah Wood <micah@wpscholar.com>
2021-08-29 16:58:14 -0400

    Update license handling and add plugin base file

---
+++ wp-plugin/plugin.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * {{ name }}
+ *
+ * @package           {{ name | pascalCase }}
+ * @author            {{ author.name }}
+ * @copyright         Copyright {{ year }} by {{ author.name }} - All rights reserved.
+ * @license           {{ license.slug }}
+ *
+ * @wordpress-plugin
+ * Plugin Name:       {{ name }}
+ * Plugin URI:        {{ url }}
+ * Description:       {{ description }}
+ * Version:           1.0
+ * Requires PHP:      7.0
+ * Requires at least: 5.0
+ * Author:            {{ author.name }}
+ * Author URI:        {{ author.url }}
+ * Text Domain:       {{ text_domain }}
+ * Domain Path:       /languages
+ * License:           {{ license.name }}
+ * License URI:       {{ license.url }}
+ */
+
